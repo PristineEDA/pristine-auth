87afb71a3b71cef34302f8f758d47a760b76c542
maksyuki <maksyuki@126.com>
2026-04-18 17:50:55 +0800

    refactor: remove all module

--- __tests__/setup.ts
+++
@@ -1,8 +0,0 @@
-import "@testing-library/jest-dom/vitest";
-
-// Polyfill ResizeObserver for jsdom (required by input-otp)
-global.ResizeObserver = class ResizeObserver {
-  observe() {}
-  unobserve() {}
-  disconnect() {}
-};
